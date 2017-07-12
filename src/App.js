a2d2e2b92df570d33f151d30e8a2ddddfadba4c6
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2017-07-12 18:29:02 +0200

    Added a new example working with an array of data

--- src/App.js
+++ src/App.js
@@ -2,17 +2,48 @@ import React from 'react';
 
 import PersonalData from './PersonalData';
 
+const contacts = [
+  {
+    name: 'Alejandro',
+    phone: 8938948934,
+    fav: true,
+    group: 'Family'
+  },
+  {
+    name: 'Isra',
+    phone: 234234342,
+    group: 'Family'
+  },
+  {
+    name: 'Rose',
+    phone: 8484884
+  },
+  {
+    name: 'Ine',
+    phone: 9999999999,
+    fav: true,
+    group: 'Friends'
+  },
+  {
+    name: 'Luci',
+    phone: 777743,
+    group: 'Friends'
+  }
+];
+
 class App extends React.Component {
 
   render() {
     return (
       <div className="App">
-        <PersonalData
-          name="Alex Guerrero"
-          phone={684839483}
-          fav={false}
-          group={'No Group'}
-        />
+        {contacts.map(contact =>
+          <PersonalData
+            {
+              ...contact
+            }
+            key = {contact.name}
+          />
+        )}
       </div>
     );
   }
