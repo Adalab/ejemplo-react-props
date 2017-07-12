e8d3990b7768ee8a7453d1e20fa77e94fb3b062d
Alex Guerrero <hello@alexguerrero.es>
2017-07-12 13:33:49 +0200

    Step 1: Default props y PropTypes

---
+++ src/PersonalData.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PersonalData extends React.Component {
+  printFav() {
+    if (this.props.fav) {
+      return 'Es favorito'
+    } else {
+      return 'No es favorito'
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.props.name}</h1>
+        <ul>
+          <li>{this.props.phone}</li>
+          <li>{this.printFav()}</li>
+          <li>{this.props.group}</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+PersonalData.propTypes = {
+  name: PropTypes.string.isRequired, // es requerido
+  phone: PropTypes.number, // Número opcional
+  fav: PropTypes.bool, // Booleano opcional (true o false)
+  group: PropTypes.oneOf(['Family', 'Friends', 'No Group']) // Opacional, o familia o friends
+};
+
+PersonalData.defaultProps = {
+  fav: false, // Booleano opcional (true o false)
+  group: 'No Group' // Opacional, o familia o friends
+};
+
+export default PersonalData;
